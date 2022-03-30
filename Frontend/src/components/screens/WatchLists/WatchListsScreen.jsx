de1dec585c22f82e67e3017d0f81e03fb893f8bd
Juan Carlos Miranda Carruana <jc.carruana@gmail.com>
2022-03-30 18:25:10 +0200

    SPF-18: create watchlist screen with the corresponding components

---
+++ Frontend/src/components/screens/WatchLists/WatchListsScreen.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import ScreensTemplate from '../../ScreensTemplate';
+import {SearchField} from '../../common';
+import {Grid} from '@mui/material';
+import WatchLists from './WatchLists';
+import AssetsList from './AssetsList';
+import PropTypes from 'prop-types';
+
+/**
+ * Component related to the watchLists page
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const WatchListsScreen = (props) => {
+  const [selectedListIndex, setSelectedListIndex] = useState(0);
+
+  const renderHeader = () => (
+    <SearchField/>
+  );
+
+  const renderBody = () => (
+    <Grid container spacing={2}>
+      <Grid item className='col-sm-5 col-md-4'>
+        <WatchLists
+          {...props}
+          watchListsArray={props.watchListsArray}
+          setWatchListsArray={props.setWatchListsArray}
+          selectedListIndex={selectedListIndex}
+          setSelectedListIndex={setSelectedListIndex}
+        />
+      </Grid>
+      <Grid item className='col-sm-12 col-md-8'>
+        <AssetsList
+          {...props}
+          watchListsArray={props.watchListsArray}
+          selectedListIndex={selectedListIndex}
+        />
+      </Grid>
+    </Grid>
+  );
+
+  return (
+    <React.Fragment>
+      <ScreensTemplate
+        headerComponent={renderHeader}
+        bodyComponent={renderBody}
+        searchBar
+        selectedNavLinkIndex={2}
+      />
+    </React.Fragment>
+
+  );
+}
+
+ScreensTemplate.propTypes = {
+  usePersistedState: PropTypes.func,
+  watchListsArray: PropTypes.array,
+  setWatchListsArray: PropTypes.func,
+};
+
+export default WatchListsScreen;
