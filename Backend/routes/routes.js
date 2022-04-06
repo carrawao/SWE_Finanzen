6532e02e46e39e0f3f108e93b7e4fe9c58cdd7df
Steffen Kruschina <steffen.kruschina@gmail.com>
2022-04-06 19:05:35 +0200

    SPF-9
    ADD:
    getShareForWatchlist backend abfrage

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -4,16 +4,19 @@ const intradayShareRoute = require('./intradayShare');
 const monthlyShareRoute = require('./monthlyShare');
 const weeklyShareRoute = require('./weeklyShare');
 
-const quotedUSshares = require('./quotedUSshares');
-const companyOverviewRoute = require('./companyOverview');
-
 //Crypto
 const dailyCryptoRoute = require('./dailyCrypto');
 const intradayCryptoRoute = require('./intradayCrypto');
 const monthlyCryptoRoute = require('./monthlyCrypto');
 const weeklyCryptoRoute = require('./weeklyCrypto');
 
+//Allgemein
+const quotedUSshares = require('./quotedUSshares');
+const companyOverviewRoute = require('./companyOverview');
 const search = require('./search');
+
+//Watchlist
+const getShareForWatchlist = require('./getShareForWatchlist');
 
 const appRouter = (app, fs, apiKey) => {
   app.get('/', (req, res) => {
@@ -35,6 +38,9 @@ const appRouter = (app, fs, apiKey) => {
   quotedUSshares(app, fs, apiKey);
   companyOverviewRoute(app, fs, apiKey);
   search(app, fs);
+
+  //Watchlist
+  getShareForWatchlist(app,fs,apiKey);
 };
 
 module.exports = appRouter;
