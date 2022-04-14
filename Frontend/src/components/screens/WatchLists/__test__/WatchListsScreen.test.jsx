88c2d10559a535ab2eb18e1bf0ae5c8a77725678
Lukas Walter <luki@MacBook-Pro-von-Lukas.local>
2022-04-14 10:17:59 +0200

    Sets watchListsArray manually in test

--- Frontend/src/components/screens/WatchLists/__test__/WatchListsScreen.test.jsx
+++ Frontend/src/components/screens/WatchLists/__test__/WatchListsScreen.test.jsx
@@ -7,6 +7,27 @@ import { BrowserRouter } from 'react-router-dom';
 
 it("Watchlist renders without crashing", () => {
     const div = document.createElement("div");
+
+    /**
+     * Get value from key in local storage
+     * @param keyName
+     * @param defaultValue
+     * @returns {any|string}
+     */
+    const persistState = (keyName, defaultValue) => {
+        const savedData = localStorage.getItem(keyName);
+        return !savedData ? defaultValue : JSON.parse(savedData);
+    };
+    
+
+    const [watchListsArray, setWatchListsArray] = useState(() => persistState('watchListsArray', []));
+    const [assetsListArray, setAssetsListArray] = useState(() => persistState('assetsListsArray', []));
+    
+    useEffect(() => {
+      localStorage.setItem('watchListsArray', JSON.stringify(watchListsArray));
+      localStorage.setItem('assetsListsArray', JSON.stringify(assetsListArray));
+    }, [watchListsArray, assetsListArray]);
+
     ReactDom.render(
         <BrowserRouter>
             <WatchListsScreen
