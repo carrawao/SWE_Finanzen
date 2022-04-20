44f822d3cd1b077bc5b6d87e56639717a9cb319f
Steffen Kruschina <steffen.kruschina@gmail.com>
2022-04-20 11:57:58 +0200

    Problem Fix
    If too many requests are made to the API, the already existing data will be overwritten. Now the data will not be overwritten and after a random time (1 to 9 minutes) the data will be tried one more time. If no file exists it will be created with "null" values.
    
    -> Daily
    -> Weekly
    -> Monthly

---
+++ Backend/module/serviceFunctions.js
@@ -0,0 +1,11 @@
+const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+//---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------//
+//exports
+module.exports = {
+    getRandomIntInclusive
+};
--- Backend/module/updateShareDataFromAPI.js
+++ Backend/module/updateShareDataFromAPI.js
@@ -3,6 +3,7 @@ const csvToJson = require('convert-csv-to-json');
 const fs = require('fs');
 const readline = require('readline');
 
+const serviceFunctions = require('./serviceFunctions');
 const pathShareSymbol = './data/shareSymbols.txt'
 let url;
 
@@ -19,7 +20,6 @@ const startUpdateShareData = async (apiKeys) => {
     // Wird erstmal weggelassen wird nicht benötigt
     // updateMonthlyShareData(apiKeys[3]);
 };
-
 //---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------//
 //Intraday Data
 async function updateIntradayShareData(apiKey) {
@@ -115,7 +115,47 @@ const updateDailySeriesShare = async (symbol, apiKey) => {
     const res = await axios.get(url)
         .then(res => res.data)
         .then(data => {
-            fs.writeFileSync(path, JSON.stringify(data));
+            try {
+                if(JSON.stringify(data).includes('Our standard API call frequency is 5 calls per minute and 500 calls per day.')){
+                    let jsonMessage = { 
+                        "Meta Data": {
+                            "1. Information": "Sorry, our API is overloaded at the moment, it may take a few minutes before your data is available.",
+                            "2. Symbol": symbol,
+                            "3. Last Refreshed": "Right now",
+                            "4. Output Size": "NONE",
+                            "5. Time Zone": "NONE"
+                        },
+                        "Time Series (Daily)": {
+                            "N0 Date": {
+                                "1. open": "0",
+                                "2. high": "0",
+                                "3. low": "0",
+                                "4. close": "0",
+                                "5. volume": "0"
+                            }
+                        }
+                    };
+
+                    if(fs.existsSync(path)){
+                        //Flie exists
+                        //Nothing todo
+                    }else{
+                        //File not exists
+                        // -> Write a new File with 
+                        fs.writeFileSync(path, JSON.stringify(jsonMessage));
+                    }
+
+                    throw new Error('To many API calls with the Key: ' + apiKey + ', for Daily Share Data');
+                }else{
+                    fs.writeFileSync(path, JSON.stringify(data));
+                }
+            } catch (error) {
+                console.error(error);
+                //After 1 to 9 minutes we will try again to get the data.
+                let randomTime = 60000 * serviceFunctions.getRandomIntInclusive(1,10);
+                setTimeout(() => updateDailySeriesShare(symbol,apiKey), randomTime);
+            }
+            
         })
         .catch(error => console.error(error))
 }
@@ -162,7 +202,46 @@ const updateWeeklySeriesShare = async (symbol, apiKey) => {
     const res = await axios.get(url)
         .then(res => res.data)
         .then(data => {
-            fs.writeFileSync(path, JSON.stringify(data));
+            try {
+                if(JSON.stringify(data).includes('Our standard API call frequency is 5 calls per minute and 500 calls per day.')){
+                    let jsonMessage = { 
+                        "Meta Data": {
+                            "1. Information": "Sorry, our API is overloaded at the moment, it may take a few minutes before your data is available.",
+                            "2. Symbol": symbol,
+                            "3. Last Refreshed": "Right now",
+                            "4. Output Size": "NONE",
+                            "5. Time Zone": "NONE"
+                        },
+                        "Time Series (Daily)": {
+                            "No Date": {
+                                "1. open": "0",
+                                "2. high": "0",
+                                "3. low": "0",
+                                "4. close": "0",
+                                "5. volume": "0"
+                            }
+                        }
+                    };
+
+                    if(fs.existsSync(path)){
+                        //Flie exists
+                        //Nothing todo
+                    }else{
+                        //File not exists
+                        // -> Write a new File with 
+                        fs.writeFileSync(path, JSON.stringify(jsonMessage));
+                    }
+
+                    throw new Error('To many API calls with the Key: ' + apiKey + ', for Weekly Share Data');
+                }else{
+                    fs.writeFileSync(path, JSON.stringify(data));
+                }
+            } catch (error) {
+                console.error(error);
+                //After 1 to 9 minutes we will try again to get the data.
+                let randomTime = 60000 * serviceFunctions.getRandomIntInclusive(1,10);
+                setTimeout(() => updateWeeklySeriesShare(symbol,apiKey), randomTime);
+            }
         })
         .catch(error => console.error(error))
 }
@@ -208,7 +287,45 @@ const updateMonthlySeriesShare = async (symbol, apiKey) => {
     const res = await axios.get(url)
         .then(res => res.data)
         .then(data => {
-            fs.writeFileSync(path, JSON.stringify(data));
+            try {
+                if(JSON.stringify(data).includes('Our standard API call frequency is 5 calls per minute and 500 calls per day.')){
+                    let jsonMessage = {
+                        "Meta Data": {
+                            "1. Information": "Sorry, our API is overloaded at the moment, it may take a few minutes before your data is available.",
+                            "2. Symbol": symbol,
+                            "3. Last Refreshed": "Right now",
+                            "4. Time Zone": "NONE"
+                        },
+                        "Monthly Time Series": {
+                            "No Date": {
+                                "1. open": "0",
+                                "2. high": "0",
+                                "3. low": "0",
+                                "4. close": "0",
+                                "5. volume": "0"
+                            }
+                        }
+                    };
+
+                    if(fs.existsSync(path)){
+                        //Flie exists
+                        //Nothing todo
+                    }else{
+                        //File not exists
+                        // -> Write a new File with 
+                        fs.writeFileSync(path, JSON.stringify(jsonMessage));
+                    }
+
+                    throw new Error('To many API calls with the Key: ' + apiKey + ', for Monthly Share Data');
+                }else{
+                    fs.writeFileSync(path, JSON.stringify(data));
+                }
+            } catch (error) {
+                console.error(error);
+                //After 1 to 9 minutes we will try again to get the data.
+                let randomTime = 60000 * serviceFunctions.getRandomIntInclusive(1,10);
+                setTimeout(() => updateMonthlySeriesShare(symbol,apiKey), randomTime);
+            }
         })
         .catch(error => console.error(error))
 }
