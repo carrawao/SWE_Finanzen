99502ff1b99c1762097afb93de567db7278b10b7
Juan Carlos Miranda Carruana <jc.carruana@gmail.com>
2022-04-30 00:21:42 +0200

    SPF-93: removing the failing test for search filed since it was already considered under watchlist testing

--- Frontend/src/components/screens/Settings/__test__/SettingsScreen.test.jsx
+++ Frontend/src/components/screens/Settings/__test__/SettingsScreen.test.jsx
@@ -4,11 +4,11 @@ import SettingsScreen from '../SettingsScreen';
 import { BrowserRouter } from 'react-router-dom';
 
 
-it("SettingsScreen renders without crashing", () => {
+it('SettingsScreen renders without crashing', () => {
     const div = document.createElement("div");
     ReactDom.render(
         <BrowserRouter>
-            <SettingsScreen></SettingsScreen>
+            <SettingsScreen />
         </BrowserRouter>
     , div);
 })
