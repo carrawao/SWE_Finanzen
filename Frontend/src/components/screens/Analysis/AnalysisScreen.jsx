e735d5396a8a588386788c500dbaaebeb028126b
Juan Carlos Miranda Carruana <jc.carruana@gmail.com>
2022-05-03 23:48:25 +0200

    SPF-96: fixing some layout issues of the analysis screen

--- Frontend/src/components/screens/Analysis/AnalysisScreen.jsx
+++ Frontend/src/components/screens/Analysis/AnalysisScreen.jsx
@@ -1,53 +1,48 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ScreensTemplate from '../../ScreensTemplate';
-import {Grid,
-        Button
-    } from '@mui/material';
+import {Grid} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 
 import AnalysisList from './AnalysisList';
 import AnalysisStockSplit from './AnalysisStockSplit'
-import AnalysisDetailItem from './AnalysisDetailitem';
 
 
-const AnalysisScreen = () => {
-
-    const renderHeader = () => (
+const AnalysisScreen = props => {
+  const renderBody = () => (
+    <Grid container className='d-flex justify-content-center pt-2'>
+      <Grid item className='col-12 col-md-5 col-xl-3'>
         <Typography variant='h6' noWrap component='div'>
-        Header of Analysis Page
+            Placeholder piechart
         </Typography>
-    );
+      </Grid>
+      <Grid item className='col-12 col-md-7 col-xl-9'>
+        <AnalysisList />
+      </Grid>
+      <Grid item xs={12} className='col-12 col-md-12 col-xl-12'>
+        <AnalysisStockSplit />
+      </Grid>
+    </Grid>
+  );
 
-    const renderBody = () => (
-        <Grid container className='d-md-flex flex-md-row justify-content-lg-around px-lg-2 px-xl-3 justify-content-center pt-2'>
-            <Grid item className='col-12 col-md-5 col-xl-3'>
-                <Typography variant='h6' noWrap component='div'>
-                    Placeholder piechart
-                </Typography>
-            </Grid>
-            <Grid item className='col-12 col-md-7 col-xl-9'>
-                <AnalysisList></AnalysisList>
-            </Grid>
-            <Grid item xs={12} className='col-12 col-md-12 col-xl-12'>
-                <AnalysisStockSplit></AnalysisStockSplit>
-            </Grid>
-            
-           
-        </Grid>   
-         
-    );
-
-    return (
-        <React.Fragment>
-        <ScreensTemplate
-            headerComponent={renderHeader}
-            bodyComponent={renderBody}
-            selectedNavLinkIndex={5}
-        />
-        </React.Fragment>
-
-    );
+  return (
+    <React.Fragment>
+      <ScreensTemplate
+        bodyComponent={renderBody}
+        selectedNavLinkIndex={5}
+        assetsListArray={props.assetsListArray}
+        searchResult={props.searchResult}
+        setSearchResult={props.setSearchResult}
+      />
+    </React.Fragment>
+  );
 }
+
+AnalysisScreen.propTypes = {
+  searchResult: PropTypes.array,
+  setSearchResult: PropTypes.func,
+  watchListsArray: PropTypes.array,
+  assetsListArray: PropTypes.array
+};
 
 export default AnalysisScreen;
