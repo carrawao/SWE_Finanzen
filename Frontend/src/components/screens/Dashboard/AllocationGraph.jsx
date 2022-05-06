48033682c90ccfeb0cc157d28002909bbe0d39f3
cedscho <93663333+cedscho@users.noreply.github.com>
2022-05-06 10:59:53 +0200

    Update AllocationGraph.jsx

--- Frontend/src/components/screens/Dashboard/AllocationGraph.jsx
+++ Frontend/src/components/screens/Dashboard/AllocationGraph.jsx
@@ -1,8 +1,5 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
-import { Typography, Grid, Paper } from '@mui/material';
 import { DoughnutChart } from '../../common';
 
 /**
@@ -16,16 +13,6 @@ const AllocationGraph = (props) => {
 
   const defaultMiddleDisplayLabel = "Total Value";
   const defaultMiddleDisplayValue = props.portfolioData.value;
-  
-  const [middleDisplayLabel, setMiddleDisplayLabel] = useState(defaultMiddleDisplayLabel);
-  const [middleDisplayValue, setMiddleDisplayValue] = useState(defaultMiddleDisplayValue);
-
-  ChartJS.register(ArcElement, Tooltip, Legend);
-
-  const setDefaultValues = () => {
-    setMiddleDisplayLabel(defaultMiddleDisplayLabel);
-    setMiddleDisplayValue(defaultMiddleDisplayValue);
-  }
   
   const assets = props.getAllAssets();
   
@@ -44,46 +31,13 @@ const AllocationGraph = (props) => {
     });
     return valueData;
   })();
-
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'value',
-        data: valueData,
-        color: [
-          'rgba(59, 151, 210, 1)',
-          'rgba(241, 155, 31, 1)',
-          'rgba(229, 126, 37, 1)',
-          'rgba(239, 195, 25, 1)',
-          'rgba(78, 185, 111, 1)',
-        ],
-        backgroundColor: [
-          'rgba(59, 151, 210, 1)',
-          'rgba(241, 155, 31, 1)',
-          'rgba(229, 126, 37, 1)',
-          'rgba(239, 195, 25, 1)',
-          'rgba(78, 185, 111, 1)',
-        ],
-        borderColor: [
-          'rgba(59, 151, 210, 1)',
-          'rgba(241, 155, 31, 1)',
-          'rgba(229, 126, 37, 1)',
-          'rgba(239, 195, 25, 1)',
-          'rgba(78, 185, 111, 1)',
-        ],
-        borderWidth: 1,
-        spacing: 0
-      },
-    ],
-  };
   
   return (
     <DoughnutChart
-      data= {valueData}
-      labels = {labels}
-      defaultMiddleDisplayValue = {defaultMiddleDisplayValue}
-      defaultMiddleDisplayLabel = {defaultMiddleDisplayLabel}
+      data={valueData}
+      labels={labels}
+      defaultMiddleDisplayValue={defaultMiddleDisplayValue}
+      defaultMiddleDisplayLabel={defaultMiddleDisplayLabel}
     ></DoughnutChart>
   );        
 }
