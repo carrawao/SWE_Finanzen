da37e7be4a00e77923c6c6358463f0fee6250cd6
Lukas Walter <luki@MacBook-Pro-von-Lukas.local>
2022-05-12 11:06:07 +0200

    Changes how Typ Allocation is calculated, changes einstellungen to settings in SettingsScreen headline, format AnalysisScreen code, comment analysisScreen functions

--- Frontend/src/components/screens/Settings/SettingsScreen.jsx
+++ Frontend/src/components/screens/Settings/SettingsScreen.jsx
@@ -31,7 +31,7 @@ const SettingsScreen = props => {
              }}
             
             >
-                    Einstellungen <SettingsIcon></SettingsIcon>
+                    Settings <SettingsIcon></SettingsIcon>
       </Typography>
 
         </Box>
